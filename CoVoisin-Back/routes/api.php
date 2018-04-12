ee9d4579b31e426961efc19cca61930ccd5d35e1
jean-christian <jc.vizzi@free.fr>
2018-04-12 17:15:09 +0200

    Changement pour permettre à Laravel d'accepter les requêtes cross origine

--- CoVoisin-Back/routes/api.php
+++ CoVoisin-Back/routes/api.php
@@ -18,32 +18,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 });
 
-Route::get('users', 'UserController@index');
-Route::post('users', 'UserController@create');
-Route::get('users/{user}', "UserController@show");
-Route::put('users/{user}', "UserController@update");
-Route::delete('users/{user}', "UserController@delete");
+Route::get('users', 'UserController@index')->middleware('cors');
+Route::post('users', 'UserController@create')->middleware('cors');
+Route::get('users/{user}', "UserController@show")->middleware('cors');
+Route::put('users/{user}', "UserController@update")->middleware('cors');
+Route::delete('users/{user}', "UserController@delete")->middleware('cors');
 
-Route::get('offers', 'OffersController@index');
-Route::get('offers/{offer}', "OffersController@show");
-Route::post('offers', 'OffersController@create');
-Route::put('offers/{offer}', "OffersController@update");
-Route::delete('offers/{offer}', "OffersController@delete");
+Route::get('offers', 'OffersController@index')->middleware('cors');
+Route::get('offers/{offer}', "OffersController@show")->middleware('cors');
+Route::post('offers', 'OffersController@create')->middleware('cors');
+Route::put('offers/{offer}', "OffersController@update")->middleware('cors');
+Route::delete('offers/{offer}', "OffersController@delete")->middleware('cors');
 
-Route::get('requests', 'RequestsController@index');
-Route::post('requests', 'RequestsController@create');
-Route::get('requests/{requests}', "RequestsController@show");
-Route::put('requests/{requests}', "RequestsController@update");
-Route::delete('requests/{requests}', "RequestsController@delete");
+Route::get('requests', 'RequestsController@index')->middleware('cors');
+Route::post('requests', 'RequestsController@create')->middleware('cors');
+Route::get('requests/{requests}', "RequestsController@show")->middleware('cors');
+Route::put('requests/{requests}', "RequestsController@update")->middleware('cors');
+Route::delete('requests/{requests}', "RequestsController@delete")->middleware('cors');
 
-Route::get('offerResponses', 'OfferResponsesController@index');
-Route::get('offerResponses/{offerResponse}', "OfferResponsesController@show");
-Route::post('offerResponses', 'OfferResponsesController@create');
-Route::put('offerResponses/{offerResponse}', "OfferResponsesController@update");
-Route::delete('offerResponses/{offerResponse}', "OfferResponsesController@delete");
+Route::get('offerResponses', 'OfferResponsesController@index')->middleware('cors');
+Route::get('offerResponses/{offerResponse}', "OfferResponsesController@show")->middleware('cors');
+Route::post('offerResponses', 'OfferResponsesController@create')->middleware('cors');
+Route::put('offerResponses/{offerResponse}', "OfferResponsesController@update")->middleware('cors');
+Route::delete('offerResponses/{offerResponse}', "OfferResponsesController@delete")->middleware('cors');
 
-Route::get('requestResponses', 'RequestResponsesController@index');
-Route::get('requestResponses/{requestResponses}', "RequestResponsesController@show");
-Route::post('requestResponses', 'RequestResponsesController@create');
-Route::put('requestResponses/{requestResponses}', "RequestResponsesController@update");
-Route::delete('requestResponses/{requestResponses}', "RequestResponsesController@delete");
+Route::get('requestResponses', 'RequestResponsesController@index')->middleware('cors');
+Route::get('requestResponses/{requestResponses}', "RequestResponsesController@show")->middleware('cors');
+Route::post('requestResponses', 'RequestResponsesController@create')->middleware('cors');
+Route::put('requestResponses/{requestResponses}', "RequestResponsesController@update")->middleware('cors');
+Route::delete('requestResponses/{requestResponses}', "RequestResponsesController@delete")->middleware('cors');
